067a89955be614b31132e6d2ff464ae34ee9fea9
ponleukit <sreanponleu18@kit.edu.kh>
2020-12-16 12:48:00 +0700

    variable and value

--- variable_and_value.js
+++ variable_and_value.js
@@ -1,5 +1,39 @@
-//  Variable ជាអញ្ញត្តិតាងមួយ
-//  Value ជាតម្លៃដែលត្រូវផ្ដល់ឲ្យអញ្ណត្តិនោះ
-var a = 2; 
-// ដែរ​​ a​ ជាvariable 2​ជា​value
+ /*
+ Variable ជាអញ្ញត្តិតាងមួយ
+ Value ជាតម្លៃដែលត្រូវផ្ដល់ឲ្យអញ្ណត្តិនោះ
+ ដែរ​​ a​ ជាvariable 2​ជា​value
+ */
 
+var a = 2; 
+
+//  The different between var, let and const
+
+/* var keyword first come with javascript
+
+  console.log(b);
+  var  b = 2;
+
+  output will be undefine
+_________________________ above code will be convert to below code when we use var keyword
+  var b;
+  console.log(b);
+  b = 2;
+
+*/
+
+/*  let keyword  was created to show the above problem (ES6)
+
+    console.log(b);
+    let b = 2;
+
+    output will be error 
+ 
+*/
+
+/* const 
+   const b = 6;
+   b = 4;
+   console.log(b);
+   
+   output will be error
+*/
