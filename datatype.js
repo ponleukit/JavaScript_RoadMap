ea9c4b432f86d970e09fabd14fb93ff155a49e98
ponleukit <sreanponleu18@kit.edu.kh>
2020-12-16 13:00:42 +0700

    datatpye

---
+++ datatype.js
@@ -0,0 +1,28 @@
+/**
+ *  There are two of data type: 
+ *   - primative datatype :  string, int, float, boolean.....
+ *   - None primative datatype: array, object, and function
+ */
+
+
+//  Briefly about data type: 
+
+let str = "this is string datatype";
+let numInt = 1; // intager
+let numFloat = 1.2345; // flaot
+let auth = false; 
+
+
+// None primative datatype
+
+let arr = [1,2,3,4,5,6];
+
+let obj = {
+    firstName: 'Syan',
+    lastName: 'Ps',
+    passWord: 1234
+}
+
+let myFunction = function(){
+    return 'myFunction'
+}
