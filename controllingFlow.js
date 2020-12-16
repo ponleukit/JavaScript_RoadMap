6ebeec15a4deebcdeb5fc57fef5114ef4933390e
ponleukit <sreanponleu18@kit.edu.kh>
2020-12-16 13:51:25 +0700

    Flow

---
+++ controllingFlow.js
@@ -0,0 +1,45 @@
+/**
+ *  here we will learn how to use if if--else if ---else nest if and switch
+ * 
+ *   if(condition){
+ *     block code what we wanna do
+ *  }
+ * 
+ *  if(condition){
+ *    block code what we wanna do
+ *  }else if(conditon){
+ *     what we wanna do if the condition is true
+ *  }else
+ *  {
+ *     block of code handle something when all the conditon is wrong
+ *  }
+ *  
+ *  switch(expression) {
+        case x:
+            // code block
+            break;
+        case y:
+            // code block
+            break;
+        default:
+            // code block
+    } 
+ */
+
+
+ // important:  all variable always has boolean valeu on it whatever datatype it is
+
+ let a = 'string';
+
+ if(a){
+     console.log('a has boolean value');
+ }
+
+ // output will be a has boolean value
+
+ /**
+  *    0, null, false, undefine : has  false value
+  *    others except above has true value
+  */
+
+  
