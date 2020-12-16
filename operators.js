569c58a3408bfedaf2c63c6bb049dd12e3021437
ponleukit <sreanponleu18@kit.edu.kh>
2020-12-16 13:25:24 +0700

    operators

---
+++ operators.js
@@ -0,0 +1,54 @@
+/**
+ * operator use to perform the operation in code
+ * There are many kind of operations such asign operator, Arithmetic Operators, Comparison Operators...
+ */
+
+let a = 2;
+let  b = 4;
+
+
+
+//Arithmetic Operators
+
+let c = b - a; // substraction operator
+    c = b + a ; // addition operator
+    c = b * a; // multiplication operator\
+    c = b / a; // division
+    c = b**a ; // expo
+    c = b % a; //modulu
+
+// Assignment operator
+/**
+ *  = 
+ *  +=
+ *  -=
+ *  *=
+ *  /=
+ *  **=
+ * 
+ */
+
+// Comparison Operators
+c == b; // compare only value but note data type    equal operator
+c === b; // compare both value and datatype         equal operator
+/**
+ *      !=   not equal
+ *      !==  not equal both value and type
+ *      < less than
+ *      <= lesss than equal
+ *      > greater than 
+ *      >= greater than and equal
+ *      ternary operator
+ *      
+ */
+
+ //Logical Operators
+
+ /**
+  *  && and 
+  *  || or
+  *   ! not
+  */
+
+
+
