09ab31d7a85defd752b6d8f24248d95d589d4d44
ponleukit <sreanponleu18@kit.edu.kh>
2020-12-18 21:10:40 +0700

    function with Argument

--- functions/functionWithargument.js
+++ functions/functionWithargument.js
@@ -7,3 +7,28 @@ function welcome(userName){                 //userName here is called parameter
 }
 
 welcome('Syan');                            // Syan is called argument
+
+
+
+//function with multiple arguments
+
+function mult(userName, passWord){
+    let passWordLength = passWord.length;
+    if(userName && passWordLength >=8){
+        console.log(`welcome ${userName}`);
+    }
+}
+
+mult('Syan', 'Syen1234@ml');
+
+
+// as for ES6 we have other way for using this kind of multiple arguments
+
+function newMult(...args){
+    let checkpassLength = args[1];
+    if(args[0] && checkpassLength){
+        console.log(`welcome ${args[0]}`)
+    }
+}
+
+newMult('Syan', 'Syen1234@ml');
