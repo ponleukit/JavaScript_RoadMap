c9d7fdeb86adc95a40c611f60c206aacc5d6a6b2
ponleukit <sreanponleu18@kit.edu.kh>
2020-12-18 20:42:03 +0700

    first function

--- functions/ourFirstFunction.js
+++ functions/ourFirstFunction.js
@@ -1,8 +1,8 @@
 // We can call function is the block of code use for perform the particular task. function chuck the complex code into small which easy for maintainance.
 // here we will create our first function by using function key word
 
-function hello(){
+function hello(){                               // we call this function as function declaration
     console.log('welcome to function section')
 };
 
-hello();
+hello();                                        // we call this function as function calling
