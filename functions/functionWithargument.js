68801b0ae86baa38dd0084a3d4e30445a04a098f
ponleukit <sreanponleu18@kit.edu.kh>
2020-12-18 20:52:40 +0700

    functionWithArgument

---
+++ functions/functionWithargument.js
@@ -0,0 +1,9 @@
+// argument: is passed by the calling function
+// parameter: A parameter is in the called function.
+
+
+function welcome(userName){                 //userName here is called parameter
+    console.log(`Welcome ${userName}`);
+}
+
+welcome('Syan');                            // Syan is called argument
