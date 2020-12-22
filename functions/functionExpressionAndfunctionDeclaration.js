1ec2070e1cf833e3dceb14e02bd6a419b41ec40c
ponleukit <sreanponleu18@kit.edu.kh>
2020-12-22 14:20:34 +0700

    function declaration adn expression

---
+++ functions/functionExpressionAndfunctionDeclaration.js
@@ -0,0 +1,15 @@
+// function expression 
+
+const ex = function(){
+    console.log("this is function expression..!");
+}
+ex();
+
+
+// function declaration use function key word
+
+function de(){
+    console.log("this is the function declaration")
+}
+
+de();
